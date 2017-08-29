13afeff4e8c843afef1f15c81b6d4bd54012086d
xiezhq <xiezhq@shanghaitech.edu.cn>
2017-08-30 03:07:17 +0800

    62ms, 87.49%

--- src/most_water_11.py
+++ src/most_water_11.py
@@ -7,9 +7,16 @@ class Solution(object):
         left, right = 0, len(height) - 1
         flag, water = 0, 0
         while (right - left) > 0:
-            short = min(height[left], height[right])
-            water = max(water, short * (right - left))
-            right -= (height[right] <= flag or height[right] <= short)
-            left += (height[left] <= flag or height[left] <= short)
-            flag = max(short, flag)
+            if height[left] <= flag or height[right] <= flag:
+                left += height[left] <= flag
+                right -= height[right] <= flag
+            elif height[left] <= height[right]:
+                flag = height[left]
+                water = max(water, height[left] * (right - left))
+                right -= height[right] == height[left]
+                left += 1
+            else:
+                flag = height[right]
+                water = max(water, height[right] * (right - left))
+                right -= 1
         return water
