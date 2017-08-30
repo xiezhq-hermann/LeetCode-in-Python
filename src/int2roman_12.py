fe30185019f6b72e71a27789832a71960c063c45
xiezhq <xiezhq@shanghaitech.edu.cn>
2017-08-30 15:48:10 +0800

    99ms, 91.59%

--- src/int2roman_12.py
+++ src/int2roman_12.py
@@ -4,9 +4,7 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
-        thousand, hundred, ten, one = num // 1000, (num //
-                                                    100) % 10, (num // 10) % 10, num % 10
-        roman = 'M' * thousand + ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'][hundred] + ['', 'X', 'XX',
-                                                                                                          'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'][ten] + ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'][one]
-
-        return roman
+        return ['', 'M', 'MM', 'MMM'][num // 1000]\
+            + ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'][(num // 100) % 10]\
+            + ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'][(num // 10) % 10]\
+            + ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'][num % 10]
