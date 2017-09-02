7c53e29a9650677c7c1b42e18ea489cfaf52fff0
xiezhq <xiezhq@shanghaitech.edu.cn>
2017-09-02 20:31:13 +0800

    35ms, 72.63%

--- src/letters_combination_17.py
+++ src/letters_combination_17.py
@@ -7,9 +7,6 @@ class Solution(object):
         :type digits: str
         :rtype: List[str]
         """
-        return [] if digits == '' else reduce(self.concat, digits, [''])
-
-    def concat(self, a, b):
         letter_map = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
                       '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
-        return [x + y for x in a for y in letter_map[b]]
+        return [] if digits == '' else reduce(lambda a, b: [x + y for x in a for y in letter_map[b]], digits, [''])
