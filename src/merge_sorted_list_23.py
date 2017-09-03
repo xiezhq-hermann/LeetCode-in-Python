3475dc636b005b231d63e0eeda8057b05aabd57b
xiezhq <xiezhq@shanghaitech.edu.cn>
2017-09-03 15:27:50 +0800

    recursive method

---
+++ src/merge_sorted_list_23.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        amount = len(lists)
+        interval = 1
+        while interval < amount:
+            for i in range(0, max(1, amount - amount % (interval * 2)), interval * 2):
+                lists[i] = self.merge2Lists(lists[i], lists[i + interval])
+            interval *= 2
+        return lists[0] if amount > 0 else lists
+
+    def merge2Lists(self, list1, list2):
+        if not list1:
+            return list2
+        if not list2:
+            return list2
+
+        if list1.val <= list2.val:
+            list1.next = self.merge2Lists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.merge2Lists(list2.next, list1)
+            return list2
