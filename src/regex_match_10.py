4220bd406766c23a096dadf70fa9084dec2fc298
xiezhq <xiezhq@shanghaitech.edu.cn>
2017-09-07 00:50:06 +0800

    56ms, 97.96%

--- src/regex_match_10.py
+++ src/regex_match_10.py
@@ -5,68 +5,38 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        if p == "":
-            return s == ""
-        else:
-            j = 0
-            for i in range(len(p)):
-                if i == len(p) - 1 or p[i + 1] != '*':
-                    if len(s) == 0 or (p[i] != s[j] and p[i] != '.'):
-                        return False
-                    else:
-                        return self.isMatch(s[j + 1:], p[i + 1:])
-                else:
-                    if self.isMatch(s[j:], p[i + 2:]):
-                        return True
-                    else:
-                        while j < len(s) and (s[j] == p[i] or p[i] == '.'):
-                            j += 1
-                            if self.isMatch(s[j:], p[i + 2:]):
-                                return True
-                        return False
-            return False
-
-class Solution(object):
-    def isMatch(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: bool
-        """
         s_len = len(s)
         p_len = len(p)
-        table = [[0]*p_len]*s_len
+        table = [[0 for i in range(p_len + 1)] for i in range(s_len + 1)]
+        back_stack = []
         i, j = 0, 0
-        while i < s_len and j < p_len:
-            
-
-        self.tried_sets = set()
-        return self.match(s, p, 0, 0)
-
-    def match(self, s, p, i, j):
-        if (i, j) in self.tried_sets:
-            return False
-        self.tried_sets.add((i,j))
-        if i == len(s) and j == len(p):
-            return True
-
-        if i == len(s):
-            if j < len(p) - 1 and p[j+1] == '*':
-                return self.match(s, p, i, j +2)
-
-        if i < len(s) and j < len(p):
-            if j < len(p) - 1 and p[j+1] == '*':
-                if self.char_match(s[i], p[j]):
-                    return self.match(s, p, i+1, j) or self.match(s, p, i, j+2)
-                else:
-                    return self.match(s, p, i, j + 2)
-            else:
-                if self.char_match(s[i], p[j]):
-                    return self.match(s, p, i + 1, j + 1)
+        while i != s_len or j != p_len:
+            if table[i][j]:
+                if len(back_stack) > 0:
+                    i, j = back_stack.pop()
+                    continue
+                return False
+            table[i][j] = 1
+            if i == s_len or j == p_len:
+                if i == s_len and j < p_len - 1 and p[j + 1] == '*':
+                    j += 2
+                elif len(back_stack) > 0:
+                    i, j = back_stack.pop()
                 else:
                     return False
-
-        return False
+                continue
+            if j < p_len - 1 and p[j + 1] == '*':
+                if self.char_match(s[i], p[j]):
+                    back_stack.append((i + 1, j))
+                j += 2
+            elif self.char_match(s[i], p[j]):
+                i, j = i + 1, j + 1
+            elif len(back_stack) > 0:
+                i, j = back_stack.pop()
+            else:
+                return False
+        else:
+            return True
 
     def char_match(self, c1, c2):
         return c1 == c2 or c2 == '.'
