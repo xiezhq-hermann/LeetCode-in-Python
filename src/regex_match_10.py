02ad035f87aec53e536202d6dd34adbd8802cdca
xiezhq <xiezhq@shanghaitech.edu.cn>
2017-09-06 15:41:18 +0800

    to be modified

--- src/regex_match_10.py
+++ src/regex_match_10.py
@@ -25,3 +25,48 @@ class Solution(object):
                                 return True
                         return False
             return False
+
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        s_len = len(s)
+        p_len = len(p)
+        table = [[0]*p_len]*s_len
+        i, j = 0, 0
+        while i < s_len and j < p_len:
+            
+
+        self.tried_sets = set()
+        return self.match(s, p, 0, 0)
+
+    def match(self, s, p, i, j):
+        if (i, j) in self.tried_sets:
+            return False
+        self.tried_sets.add((i,j))
+        if i == len(s) and j == len(p):
+            return True
+
+        if i == len(s):
+            if j < len(p) - 1 and p[j+1] == '*':
+                return self.match(s, p, i, j +2)
+
+        if i < len(s) and j < len(p):
+            if j < len(p) - 1 and p[j+1] == '*':
+                if self.char_match(s[i], p[j]):
+                    return self.match(s, p, i+1, j) or self.match(s, p, i, j+2)
+                else:
+                    return self.match(s, p, i, j + 2)
+            else:
+                if self.char_match(s[i], p[j]):
+                    return self.match(s, p, i + 1, j + 1)
+                else:
+                    return False
+
+        return False
+
+    def char_match(self, c1, c2):
+        return c1 == c2 or c2 == '.'
