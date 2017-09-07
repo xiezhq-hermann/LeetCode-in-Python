5b967f6ba65cd2b8438cbd4ea272eb98ef401281
xiezhq <xiezhq@shanghaitech.edu.cn>
2017-09-07 14:32:36 +0800

    38ms, 84.13%

--- src/search_range_34.py
+++ src/search_range_34.py
@@ -5,22 +5,25 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        low, high = 0, len(nums) - 1
-        while low <= high:
-            mid = (low + high) // 2
-            if nums[mid] == target:
-                break
-            elif nums[mid] > target:
-                high = mid - 1
-            else:
-                low = mid + 1
-        else:
-            return [-1, -1]
+        def searchLeft(low, high, target):
+            while low <= high:
+                mid = (low + high) // 2
+                if nums[mid] >= target:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            return low
 
-        low = high = mid
-        while low > 0 and nums[low - 1] == target:
-            low -= 1
-        while high < len(nums) - 1 and nums[high + 1] == target:
-            high += 1
+        def searchRight(low, high, target):
+            while low <= high:
+                mid = (low + high) // 2
+                if nums[mid] <= target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            return high
 
-        return [low, high]
+        left = searchLeft(0, len(nums) - 1, target)
+        if left < len(nums) and nums[left] == target:
+            return [left, searchRight(0, len(nums) - 1, target)]
+        return [-1, -1]
