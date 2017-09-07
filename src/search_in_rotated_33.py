7808d387f712ae9d1dd6d925cd538c2107def1b7
xiezhq <xiezhq@shanghaitech.edu.cn>
2017-09-07 13:22:42 +0800

    35ms, 84.31%

--- src/search_in_rotated_33.py
+++ src/search_in_rotated_33.py
@@ -5,5 +5,21 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
 
-        return nums.index(target) if target in nums else -1 
+            if nums[low] <= target:
+                if nums[mid] > target or nums[mid] <= nums[high] < nums[low]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if target > nums[mid] or nums[mid] >= nums[low] > nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        else:
+            return -1
