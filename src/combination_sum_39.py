412c78b6b0f98bfe22b24fc1388dbbb57e2b5856
xiezhq <xiezhq@shanghaitech.edu.cn>
2017-09-09 01:01:25 +0800

    78ms, 95.26%

--- src/combination_sum_39.py
+++ src/combination_sum_39.py
@@ -1,31 +1,16 @@
 class Solution(object):
     def combinationSum(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-        ref_set = set(candidates)
+        candidates.sort()
         table = [[] for _ in range(target + 1)]
-
         for i in range(1, target + 1):
-            tag = 1
-            flag = 1  # a flag for determine range
-            while tag <= i // 2:
-                if table[tag] and table[i - tag]:
-                    if flag:
-                        table[i] = [m + n for m in table[tag]
-                                    for n in table[i - tag]]
-                        flag = 0
-                    elif tag in ref_set:
-                        for combination in table[i - tag][::-1]:
-                            if combination[0] >= tag:
-                                table[i].append([tag] + combination)
-                            else:
-                                break
-                tag += 1
-
-            if i in ref_set:
-                table[i].append([i])
-
+            for number in candidates:
+                if number >= i:
+                    if number == i:
+                        table[i].append([i])
+                    break
+                for L in table[i - number]:
+                    if number >= L[-1]:
+                        table[i].append(L + [number])
+                    else:
+                        break
         return table[target]
