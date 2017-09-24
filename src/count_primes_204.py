c2a6f3b4e8dc084aeff3211a602470dd8cb6c088
xiezhq <xiezhq@shanghaitech.edu.cn>
2017-09-24 12:47:43 +0800

    662ms, 69.43%

--- src/count_primes_204.py
+++ src/count_primes_204.py
@@ -4,14 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        if n < 3:
+            return 0
         primes = [1] * n
-        count = n - 2
 
         for i in range(2, int(n**0.5) + 1):
             if primes[i]:
                 for j in range(i * i, n, i):
-                    if primes[j]:
-                        count -= 1
                     primes[j] = 0
 
-        return max(count, 0)
+        return sum(primes) - 2
