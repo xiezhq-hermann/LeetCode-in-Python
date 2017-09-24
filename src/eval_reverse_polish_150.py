6410aceb5457f550c099da0c300355d7fa430cf1
xiezhq <xiezhq@shanghaitech.edu.cn>
2017-09-24 13:06:33 +0800

    62ms, 75.09%

---
+++ src/eval_reverse_polish_150.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        token_stack = []
+        for t in tokens:
+            if t not in ["+", "-", "*", "/"]:
+                token_stack.append(int(t))
+            else:
+                r, l = token_stack.pop(), token_stack.pop()
+                if t == "+":
+                    token_stack.append(l+r)
+                elif t == "-":
+                    token_stack.append(l-r)
+                elif t == "*":
+                    token_stack.append(l*r)
+                else:
+                    if r == 0:
+                        raise ZeroDivisionError("integer division by zero")
+                    else:
+                        if l*r < 0 and l % r != 0:
+                            token_stack.append(l/r+1)
+                        else:
+                            token_stack.append(l/r)
+        return token_stack.pop()
