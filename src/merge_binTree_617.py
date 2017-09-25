e5fa30a8db5eabef1f520dc70713df7a92f4e976
xiezhq <xiezhq@shanghaitech.edu.cn>
2017-09-25 16:25:00 +0800

    109ms, 85.47%

--- src/merge_binTree_617.py
+++ src/merge_binTree_617.py
@@ -13,19 +13,12 @@ class Solution(object):
         :type t2: TreeNode
         :rtype: TreeNode
         """
-        if t1 and t2:
+        if not t1:
+            return t2
+        elif not t2:
+            return t1
+        else:
             root = TreeNode(t1.val + t2.val)
             root.left = self.mergeTrees(t1.left, t2.left)
             root.right = self.mergeTrees(t1.right, t2.right)
-        elif t1:
-            root = TreeNode(t1.val)
-            root.left = self.mergeTrees(t1.left, None)
-            root.right = self.mergeTrees(t1.right, None)
-        elif t2:
-            root = TreeNode(t2.val)
-            root.left = self.mergeTrees(None, t2.left)
-            root.right = self.mergeTrees(None, t2.right)
-        else:
-            return None
-
-        return root
+            return root
