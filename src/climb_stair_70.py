9d7d33f341741de0feacad92d3137d22292941b9
xiezhq <xiezhq@shanghaitech.edu.cn>
2017-10-01 11:46:33 +0800

    32ms, 59.17%

--- src/climb_stair_70.py
+++ src/climb_stair_70.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        base, res = 1, 1
+        base = res = 1
         for a in range(2, n + 1):
             base, res = res, base + res
         return res
