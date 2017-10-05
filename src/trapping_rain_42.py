3a9fa244eaa1368dbecb29d96b806d201bdbca5d
xiezhq <xiezhq@shanghaitech.edu.cn>
2017-10-06 02:14:06 +0800

    42ms, 81.65%

--- src/trapping_rain_42.py
+++ src/trapping_rain_42.py
@@ -4,30 +4,19 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        if not height:
-            return 0
-        water = flag = gradient = 0
-        left = right = height[0]
-        tem = [left]
-        height.append(0)
-        for i in height[1:]:
-            gradient = i - right
-            if gradient >= 0 or flag:
-                if gradient >= 0 :
-                    flag = 0
-                tem.append(i)
+        left, right = 0, len(height) - 1
+        water = left_max = right_max = 0
+        while left <= right:
+            if height[left] <= height[right]:
+                if left_max <= height[left]:
+                    left_max = height[left]
+                else:
+                    water += left_max - height[left]
+                left += 1
             else:
-                left_inx, right_inx = 0, -1
-                if left > right:
-                    while tem[left_inx] > right:
-                        left_inx += 1
-                        left = tem[left_inx - 1]
-                if left < right:
-                    while tem[right_inx - 1] > left:
-                        right_inx -= 1
-                water += min(left, tem[right_inx])*len(tem[left_inx:right_inx])-sum(tem[left_inx:right_inx])
-                flag = 1
-                tem = [right, i]
-                left = right
-            right = i
+                if right_max <= height[right]:
+                    right_max = height[right]
+                else:
+                    water += right_max - height[right]
+                right -= 1
         return water
