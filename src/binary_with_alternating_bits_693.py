6c5ef4ca3feb61993d6d431a653657e1993706cf
xiezhq <xiezhq@shanghaitech.edu.cn>
2017-10-08 18:38:51 +0800

    69ms, no rate yet, concise solution

--- src/binary_with_alternating_bits_693.py
+++ src/binary_with_alternating_bits_693.py
@@ -4,5 +4,6 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        check = n ^ (n >> 1)
+        check = n ^ (n >> 1)  # check the adjecent two bits
+        # if there is no alternative bits, all bits of check will be 1
         return not check & (check + 1)
